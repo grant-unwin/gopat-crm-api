07294f5ee3d7bab789019ebcdc38a5562eb0e365
Grant <grant@grantunw.in>
2021-02-08 01:00:22 +0000

    morer migs

---
+++ Gopat.Crm/Gopat.Crm.Models/Migrations/20210208010005_AddFkContrainsts2.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Gopat.Crm.Models.Migrations
+{
+    public partial class AddFkContrainsts2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contracts_Companies_CompanyId",
+                table: "Contracts");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Contracts_Companies_CompanyId",
+                table: "Contracts",
+                column: "CompanyId",
+                principalTable: "Companies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contracts_Companies_CompanyId",
+                table: "Contracts");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Contracts_Companies_CompanyId",
+                table: "Contracts",
+                column: "CompanyId",
+                principalTable: "Companies",
+                principalColumn: "Id");
+        }
+    }
+}
